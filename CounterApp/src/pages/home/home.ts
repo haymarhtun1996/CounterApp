1cb072f31eb34d3de13d9f176dca4a488231ed60
Thu-Rein-Htun <sexymalay1996@gmail.com>
2017-05-05 09:54:48 +0630

    Counter Testing

---
+++ CounterApp/src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  selector:'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public tapCounter:number=0;
+  constructor (public navCtrl:NavController){
+
+  }
+
+  buttonTapped(){
+    this.tapCounter++;
+  }
+  reset(){
+    this.tapCounter--;
+  }
+  
+}
